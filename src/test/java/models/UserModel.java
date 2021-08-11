5578714dd95cd43cb3a7868b0bdaa72a1d28c554
Narek <narek.ntm@gmail.com>
2021-08-12 02:35:45 +0400

    Update UserModel
    - Add getUserData method which parses Response and returns UserModel object

--- src/test/java/models/UserModel.java
+++ src/test/java/models/UserModel.java
@@ -1,12 +1,23 @@
 package models;
 
+import io.restassured.response.Response;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserModel {
     private int id;
     private String name;
     private String email;
     private String gender;
     private String status;
+
+    public static UserModel getUserData(Response response) {
+        return response
+                .jsonPath()
+                .getObject("data", UserModel.class);
+    }
 }
